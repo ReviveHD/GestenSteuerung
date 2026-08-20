61a1c7bee4e59890f2c2ec399e1eb8eba91cd90c
ReviveHD <148337572+ReviveHD@users.noreply.github.com>
2026-08-20 18:15:19 +0200

    Update config.py
    deleting personal spotify keys

--- core/config.py
+++ core/config.py
@@ -1,9 +1,9 @@
 # config.py
 
 # Spotify API Zugangsdaten
-SPOTIFY_CLIENT_ID = "22b43aac4d984f4291be99ed26b031b8"
-SPOTIFY_CLIENT_SECRET = "ae15b06bf81a4e3ba1465e462e85309d"
-SPOTIFY_REDIRECT_URI = "http://127.0.0.1:8888/callback"
+SPOTIFY_CLIENT_ID = ""
+SPOTIFY_CLIENT_SECRET = ""
+SPOTIFY_REDIRECT_URI = ""
 
 # Gesten-Konfiguration
 GESTURE_COOLDOWN = 5.0  # Sekunden zwischen zwei Befehlen
